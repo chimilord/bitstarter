241575bf9629274d3b2f54d9655337be41625f67
Ivan Carreon <ubuntu@ip-172-31-26-39.us-west-2.compute.internal>
2013-08-07 16:08:52 +0000

    HW3: Part1

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
+var data;
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  data = fs.readFileSync('index.html');
+  var buffer = new Buffer(data);
+  console.log(buffer.toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
